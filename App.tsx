b2d11c3db01e077aad9a497849568ed3756f45fa
Samuel <samuelalves681@gmail>
2023-01-19 23:50:13 -0300

    add header of summary table

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ import {
   Inter_700Bold,
   Inter_800ExtraBold,
 } from "@expo-google-fonts/inter";
-import { Loading } from "./src/components/loading";
+import { Loading } from "./src/components/Loading";
 import { Home } from "./src/screens/Home";
 
 export default function App() {
